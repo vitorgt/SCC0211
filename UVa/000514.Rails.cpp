2acc8b8f637649b4fbf2d1f3068efd14df567bfb
Vitor Gratiere Torres <26410338+vitorgt@users.noreply.github.com>
2020-08-31 10:10:10 -0300

    Add files via upload

---
+++ UVa/000514.Rails.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int INF = 0x3f3f3f3f;
+
+int main(int argc, const char **argv) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    return 0;
+}
---
+++ UVa/011340.Newspaper.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int INF = 0x3f3f3f3f;
+
+int main(int argc, const char **argv) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << fixed << setprecision(2);
+
+    int n = 0, k = 0, m = 0, val = 0;
+    long long sum = 0;
+    char c = 0;
+    string buffer = "";
+    unordered_map<char, int> mp;
+    cin >> n;
+    while (n--) {
+        cin >> k;
+        while (k--) {
+            cin >> c >> val;
+            mp[c] = val;
+        }
+        cin >> m;
+        getline(cin, buffer);
+        while (m--) {
+            getline(cin, buffer);
+            for (char c : buffer) {
+                sum += mp[c];
+            }
+        }
+        cout << sum / 100.0 << "$\n";
+        mp.clear();
+        sum = 0;
+    }
+    return 0;
+}
